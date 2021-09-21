cfbd9571ac9f0d622996543de2c11f2947a8d4fe
gbubemismith <gsmithwalter@gmail.com>
2021-09-21 16:10:18 +0100

    added dependency ioc

---
+++ Infra.IoC/MicroRabbit.Infra.IoC/DependencyContainer.cs
@@ -0,0 +1,14 @@
+using MicroRabbit.Domain.Core.Bus;
+using MicroRabbit.Infra.Bus;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace MicroRabbit.Infra.IoC
+{
+    public class DependencyContainer
+    {
+        public static void RegisterServices(IServiceCollection services)
+        {
+            services.AddTransient<IEventBus, RabbitMQBus>();
+        }
+    }
+}
